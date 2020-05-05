31d2774ab15680bee62ef8b9273c8e6a6c949550
Virginia Ladd <virginialadd2@gmail.com>
2020-05-05 14:13:23 -0600

    Attempt to fix routing

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Login.js';
-const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name='Login'
-          component={ Login }
-        />
+        <Stack.Screen name="Home" component={ Login } />
       </Stack.Navigator>
     </NavigationContainer>
   );
