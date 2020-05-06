31a7a079709c912f57c80fff5e862694b7f958fe
Virginia Ladd <virginialadd2@gmail.com>
2020-05-06 12:56:24 -0600

    Move login component into components folder for readability

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './Login.js';
+import Login from './Components/Login.js';
 
 const Stack = createStackNavigator();
 
